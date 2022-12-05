ab5de9433e93486518f51d48ec4c6a53c2ebeebe
Maikel Wever <maikelwever@gmail.com>
2022-12-05 18:40:01 +0100

    Day 5 part 2

--- year_2022/day_5.py
+++ year_2022/day_5.py
@@ -6,7 +6,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def main(data):
+def logic(data, is_mover_9001=False):
     stacks = defaultdict(list)
     moves = []
     for line in data.splitlines():
@@ -28,11 +28,19 @@ def main(data):
                     stacks[i].insert(0, letter)
 
     logger.debug("Stacks: %r", stacks)
+    stack_count_start = sum(len(i) for i in stacks.values())
 
     for move_count, stack_pop, stack_append in moves:
-        for _ in range(move_count):
-            popped = stacks[stack_pop].pop()
-            stacks[stack_append].append(popped)
+        if is_mover_9001:
+            neg_move_count = move_count * -1
+            popped = stacks[stack_pop][neg_move_count:]
+            stacks[stack_pop] = stacks[stack_pop][:neg_move_count]
+            logger.debug("Popped %d from %d: %r, new stack: %r", neg_move_count, stack_pop, popped, stacks[stack_pop])
+            stacks[stack_append].extend(popped)
+        else:
+            for _ in range(move_count):
+                popped = stacks[stack_pop].pop()
+                stacks[stack_append].append(popped)
 
     logger.debug("Re-ordered stacks: %r", stacks)
 
@@ -40,5 +48,17 @@ def main(data):
     for stack_id in sorted(stacks.keys()):
         message += stacks[stack_id][-1]
 
+    stack_count_end = sum(len(i) for i in stacks.values())
+    if stack_count_end != stack_count_start:
+        logger.warning("Sanity check failed: started with %d crates, ended up with %d crates!", stack_count_start, stack_count_end)
+
     logger.info("Message to elfs: %s", message)
+
+
+def main(data):
+    return logic(data)
+
+
+def main_2(data):
+    return logic(data, is_mover_9001=True)
 
